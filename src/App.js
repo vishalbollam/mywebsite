2170c2a51b0229cf71c5f2970869c1368c712b50
vishalbollam <vishalbollam139@gmail.com>
2021-05-02 19:25:46 -0700

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "./components/Navbar";
-
-
-
-
-
-
+import Particles from "react-particles-js";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <>
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 30,
+                density: {
+                  enable: true,
+                  value_area: 900,
+                },
+              },
+              shape: {
+                type: "star",
+                stroke: {
+                  width: 6,
+                  color: "#f9ab00",
+                },
+              },
+            },
+          }}
+        />
+        <Navbar />
+        <Header />
+      </>
     </div>
   );
 }
